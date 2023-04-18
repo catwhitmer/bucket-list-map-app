73d24bf81a71c503011917ebba47cdcd199b3af6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-04-18 09:06:26 -0400

    Added error middleware to the server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("../middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
 
@@ -9,5 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
