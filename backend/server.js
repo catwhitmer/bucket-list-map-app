8aa709c0790cdc8848ce99039796e57b6e1a2971
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-04-20 08:07:54 -0400

    Installed cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("../middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const port = process.env.PORT || 8000;
 
 connectDB();
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(errorHandler);
