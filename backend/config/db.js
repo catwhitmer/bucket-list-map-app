79c23976f37cbb13e5cb58c1ded6c692a5440004
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-04-20 15:21:27 -0400

    Fixed the mysql connection bug by installing mysql2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,15 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const connectDB = mysql.createConnection({
   host: "localhost",
   user: "cat",
   password: "password678",
   database: "bucket_map",
 });
 
-connection.connect();
-
-connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+connectDB.connect(function(err) {
   if (err) throw err;
-
-  console.log("The solution is: ", rows[0].solution);
+  console.log("Database Connected!");
 });
 
-connection.end();
+module.exports = connectDB;
