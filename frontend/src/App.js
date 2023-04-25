ab1434c6348512f406b707515c539bdfc45202ad
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-04-25 08:51:59 -0400

    Organized imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
-import "react-toastify/dist/ReactToastify.css";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
