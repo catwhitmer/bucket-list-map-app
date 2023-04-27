35d0ae3a73a426170906a301c3367c9dfc89cdd5
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-04-27 09:26:11 -0400

    Added some styling to the nav bar component and the map component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,12 @@
 function NavBar() {
-  return <div className="nav-container">Sign Up</div>;
+  return (
+    <div className="nav-container">
+      <div className="nav-links">
+        <div>Sign In</div>
+        <div>Sign Up</div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
