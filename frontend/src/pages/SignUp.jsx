560440c2e5fd5440114e657f537ff642088ac56a
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-05-02 09:22:13 -0400

    Added sign-in and sign-up forms and added CSS

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,88 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Sign Up!</p>
+      </header>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="nameInput"
+          placeholder="Name"
+          id="name"
+          value={name}
+          onChange={onChange}
+        />
+
+        <input
+          type="email"
+          className="emailInput"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={onChange}
+        />
+
+        <div className="passwordInputDiv">
+          <input
+            className="passwordInput"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+
+          <img
+            className="showPassword"
+            src=""
+            alt="Show password"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        </div>
+
+        <Link className="forgotPasswordLink" to="/forgot-password">
+          Forgot Password
+        </Link>
+
+        <div className="signUpBar">
+          <p signUpText>Sign Up</p>
+          <button className="signUpButton"></button>
+        </div>
+      </form>
+
+      <Link className="registerLink" to="/sign-in">
+        Sign In Instead
+      </Link>
+    </div>
+  );
 }
 
 export default SignUp;
