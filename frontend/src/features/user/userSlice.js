208d62ab6dff6ff0d886d151a2e4f18f765bc3d6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-05-23 09:43:58 -0400

    Updated the user slice and the store

---
+++ frontend/src/features/user/userSlice.js
@@ -0,0 +1,77 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Register new user
+export const register = createAsyncThunk(
+  "users/register",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        "api/users",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+      let data = await response.json();
+      console.log("data", data);
+
+      if (response.status === 200) {
+        localStorage.setItem("token", data.token);
+        return { ...data, email: email };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log("Error", e.response.data);
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = userSlice.actions;
+export default userSlice.reducer;
